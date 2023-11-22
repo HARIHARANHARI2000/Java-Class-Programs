94f7cfb03ff14e118b47a94e069d87d787d7877e
HARIHARANHARI08@ <shari28052000@gmail.com>
2023-11-22 12:39:27 +0530

    ...

---
+++ src/DataStructure/Welcome.java
@@ -0,0 +1,10 @@
+package DataStructure;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		String i = "welcome";
+//        input = String.join(" ", input.split(""));
+        System.out.println(i.replace("", " ").substring(1));
+    }
+}
