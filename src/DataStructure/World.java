0b9cbcb8a5bf7e47de6432a9c182c9927487114d
HARIHARANHARI08@ <shari28052000@gmail.com>
2023-11-22 14:39:32 +0530

    Training

---
+++ src/DataStructure/World.java
@@ -0,0 +1,11 @@
+package DataStructure;
+
+public class World {
+
+	public static void main(String[] args) {
+		  String i= "hello";
+	        System.out.println(i.replace("", " ").substring(1));
+	    }
+	}
+
+	
--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,9 +1,12 @@
 //import java.util.Scanner;
 //package training1;
+
+import java.util.Scanner;
+
 public class Palindrome {
 
 	public static void main(String[] args) {
-		/*Scanner s=new Scanner(System.in);
+		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
 				int temp=n,rev=0,rem;
 		while(n>0) {
@@ -15,7 +18,7 @@ public class Palindrome {
 		System.out.println("Palindrome");
 		else
 			System.out.println("Not a Palindrome");
-		s.close();*/
+		s.close();
 
 	}
 
