25ee662fb9260442b725d68a80046221d1e9a712
HARIHARANHARI08@ <shari28052000@gmail.com>
2023-11-30 14:14:28 +0530

    ProblemsTree

--- src/ProblemsTree/problem1.java
+++ src/ProblemsTree/problem1.java
@@ -9,19 +9,31 @@ public class problem1 {
 	}
 
 	public static void main(String[] args) {
-		problem1 n = new problem1(21);
-		n.left=new problem1(9);
-		n.right=new problem1(6);
-		n.left.left=new problem1(23);
+		problem1 n = new problem1(38);
+		n.left=new problem1(27);
+		n.right=new problem1(25);
+		n.left.left=new problem1(20);
 		n.left.right=new problem1(18);
-		n.left.left.left=new problem1(21);
-		n.left.left.right=new problem1(13);
-		//n.left.right.left=new problem1(3);
-		n.right.left=new problem1(7);
-		n.right.right=new problem1(16);
-		n.right.left.left=new problem1(17);
-		n.right.right.left=new problem1(15);
-		n.right.right.left.left=new problem1(11);
+		n.left.left.left=new problem1(55);
+		n.left.left.right=new problem1(24);
+	    n.left.right.left=new problem1(28);
+		n.right.left=new problem1(45);
+		n.right.right=new problem1(32);
+		n.right.left.left=new problem1(39);
+		n.right.right.left=new problem1(13);
+		n.right.right.right=new problem1(16);
+		n.right.left.right=new problem1(8);
+		n.right.left.right.right=new problem1(12);
+		n.right.left.right.right.left=new problem1(9);
+		n.right.right.right.left=new problem1(6);
+		n.right.right.right.right=new problem1(4);
+		
+		problem1traversal traversal = new problem1traversal(n);
+		traversal.inOrder();
+		System.out.println("-------------------------");
+	    traversal.preOrder();
+	    System.out.println("--------------------------");
+	    traversal.postOrder();
 		
 		
 
--- src/ProblemsTree/problem1traversal.java
+++ src/ProblemsTree/problem1traversal.java
@@ -1,5 +1,39 @@
 package ProblemsTree;
 
 public class problem1traversal {
+	problem1 n;
+	public problem1traversal(problem1 n) {
+		this.n=n;
+	}
+	public void inOrder() {
+		inOrder(n);
+	}
+	public void inOrder(problem1 currentn) {
+		if(currentn !=null) {
+			inOrder(currentn.left);
+			System.out.print(" "+currentn.data);
+			inOrder(currentn.right);
+		}
+	}
+	public void preOrder() {
+		preOrder(n);
+	}
+	public void preOrder(problem1 currentn) {
+		if(currentn !=null) {
+			preOrder(currentn.right);
+			System.out.print(" "+currentn.data);
+			preOrder(currentn.left);
+		}
+	}
+	public void postOrder() {
+		postOrder(n);
 	
 	}
+	public void postOrder(problem1 currentn) {
+		if(currentn !=null) {
+			postOrder(currentn.left);
+			postOrder(currentn.right);
+			System.out.print(" "+currentn.data);
+		}
+		}
+}
